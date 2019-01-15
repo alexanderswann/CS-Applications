898b3551f04e8df5cbd96d1f6711dc41c9d56483
AlexanderSwann <32579236+AlexanderSwann@users.noreply.github.com>
2019-01-15 14:27:52 -0500

    test

---
+++ pythonVariables.py
@@ -0,0 +1,16 @@
+num1 = 5
+num2 = 6 - 2
+num3 = num1 * num2
+#print(num3)
+
+x = 2
+y = 4
+z =7
+
+m = 2 * x
+n = (5*y)+(3*z)
+o = round((x+2)/3, 3)
+p = round((z+1)**(1/2),3)
+q = round(((((3/2)*x)-2)**(1/4))/((-3/4)*x), 3)
+
+print(m, n, o, p , q)
