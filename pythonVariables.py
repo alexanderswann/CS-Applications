df4f9254e1b9a36f897958c4e4e64830c2454093
AlexanderSwann <32579236+AlexanderSwann@users.noreply.github.com>
2019-01-24 12:48:46 -0500

    notes

--- pythonVariables.py
+++ pythonVariables.py
@@ -1,11 +1,5 @@
 # Monday, January 15, 2019
 # CS Applications
-#
-#     basic operators
-#         + , - , * , / , **(raise to power)
-#     shell
-#
-
 
 num1 = 5
 num2 = 6 - 2
@@ -24,4 +18,18 @@ q = round(((((3/2)*x)-2)**(1/4))/((-3/4)*x), 3)
 
 print(m, n, o, p , q)
 
-yup
+
+# math challenges
+import math
+print((89%7*8)%6)
+print((55//7+89)%29)
+print((3)%((17**2)*(3)))
+print((68*4)//(94%27))
+print((37*6)//(84%29))
+print(75//((17**2)//(42)))
+print(math.sqrt(48))
+print (45%19)
+print(8**5)
+print((17 ** 2) % 9 - 90)
+print ((12 * 8 / 6) % 99)
+print((-((216/27)**(1/3)))	)
