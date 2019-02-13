e5171cbf222beaab031bf435940b581ec33b062b
AlexanderSwann <32579236+AlexanderSwann@users.noreply.github.com>
2019-02-12 22:52:49 -0500

    loops and range function practice and binary addition

---
+++ alexanderSwann_loopsPractice.py
@@ -0,0 +1,76 @@
+#Alexander Swann
+#Tuesday, February 12, 2019
+#CS Applications
+
+#loopsPractice
+
+#Part 1
+# for x in range(5):
+#     print(x)
+# print()
+# for x  in range(11,15):
+#     print(x)
+# print()
+# for x in range(8,21,3):
+#     print(x)
+# print()
+# for x in range(10, 0, -1):
+#     print(x)
+# print("Blast off!")
+# print()
+# for x in range(-10,-31,-5):
+#     print(x)
+
+#Part 2
+
+# def func1(n):
+#     for x in range(0, n+1):
+#         print(x)
+#
+# def func2(n):
+#     for x in range(2, (n*2)+1, 2):
+#         print(x)
+# def func3(n, m):
+#     for x in range(n, m+1):
+#         print(x)
+#
+# func1(5)
+# func2(5)
+# func3(3,6)
+
+#challenge
+
+def add(x, y):
+    num1 = list(str('{0:01280b}'.format(x)[::-1])) #https://stackoverflow.com/questions/10411085/converting-integer-to-binary-in-python
+    num2 = list(str('{0:01280b}'.format(y)[::-1])) #https://stackoverflow.com/questions/931092/reverse-a-string-in-python
+    carry = [0]
+    answer = []
+
+    for i in range(len(num1)):
+        if num1[i] == '0' and num1[i] == num2[i] == carry[i]:
+            answer.append('0')
+            carry.append('0')
+        elif num1[i] == '1' and num1[i] == num2[i] == carry[i]:
+            answer.append('1')
+            carry.append('1')
+        elif (num1[i] == '1' and num2[i] == '1') or (num1[i] == '1' and carry[i] == '1') or (num2[i] == '1' and carry[i] == '1'):
+            answer.append('0')
+            carry.append('1')
+        elif num1[i] == '1' or num2[i] == '1' or carry[i] == '1':
+            carry.append('0')
+            answer.append('1')
+    return int(''.join(answer)[::-1],2) #https://stackoverflow.com/questions/8928240/convert-base-2-binary-number-string-to-int
+
+def mult(x, y):
+    total = 0
+    for i in range(y):
+        total = add(total, x)
+    return(total)
+
+def exp(x,y):
+    total = 1
+    for i in range(y):
+        total = mult(total, x)
+    return(total)
+
+print(add(5,89), mult(5,7), exp(5,63))
--- scratchwork.py
+++ scratchwork.py
@@ -20,10 +20,10 @@
 #     return ((a < 0 and b > 0) or (a > 0 and b < 0))
 #
 #     def not_string(str):
-#   if "not" in str[:3]:
+#   if 'not' in str[:3]:
 #     return (str)
 #   else:
-#     return( "not " + str)
+#     return( 'not ' + str)
 #
 #     def missing_char(str, n):
 #       front = str[:n]
@@ -31,7 +31,7 @@
 #       return front + back
 
 
-# animal = "Aardvark"
+# animal = 'Aardvark'
 #
 # for letter in animal:
 #     print(letter)
@@ -40,7 +40,7 @@
 # for item in aList:
 #     print(item*2)
 
-# word = "Aardvark"
+# word = 'Aardvark'
 # a = 0
 # for letter in word:
 #     if letter == 'a' or letter == 'A':
@@ -48,17 +48,17 @@
 # print(a)
 
 #with .lower method
-# word = "Aardvark"
+# word = 'Aardvark'
 # a = 0
 # for letter in word:
-#     if str.lower(letter) == "a":
+#     if str.lower(letter) == 'a':
 #         a+=1
 # print(a)
 
 # lower = 0
 # upper = 0
 # other = 0
-# word = str(input("enter a phrase "))
+# word = str(input('enter a phrase '))
 # for letter in word:
 #     if ord(letter)  >= 65 and ord(letter) <= 90:
 #         upper+=1
@@ -66,17 +66,86 @@
 #         lower+=1
 #     else:
 #         other+=1
-# print(lower,"lower", upper, "upper", other, "other")
+# print(lower,'lower', upper, 'upper', other, 'other')
 
-lower = 0
-upper = 0
-other = 0
-word = str(input("enter a phrase "))
-for letter in word:
-    if ord(letter)  <= ord('z') and ord(letter) >= ord("a"):
-        lower+=1
-    elif ord(letter)  >= ord('A') and ord(letter)  <= ord('Z'):
-        upper+=1
-    else:
-        other+=1
-print(lower,"lower", upper, "upper", other, "other")
+# lower = 0
+# upper = 0
+# other = 0
+# word = str(input('enter a phrase '))
+# for letter in word:
+#     if ord(letter)  <= ord('z') and ord(letter) >= ord('a'):
+#         lower+=1
+#     elif ord(letter)  >= ord('A') and ord(letter)  <= ord('Z'):
+#         upper+=1
+#     else:
+#         other+=1
+# print(lower,'lower', upper, 'upper', other, 'other')
+
+# y = 8
+# total = 0
+# for i in range(y):
+#     total += y
+#     print(total)
+
+# print(bin(6)[:] )
+# print(bin(6)[::-1] )
+# print('{0:08b}'.format(6)[::-1])
+# print ( int('{0:08b}'.format(6)[::-1]), 2)
+#
+# x ='{0:08b}'.format(6)[::-1]
+#
+# print(int(x,2))
+
+
+# x = 127
+# y = 63
+# num1 = list(str('{0:08b}'.format(x)[::-1]))
+# num2 = list(str('{0:08b}'.format(y)[::-1]))
+# carry = [0]
+# answer = []
+# for i in range(len(num1)):
+#     if num1[i] == '0' and num1[i] == num2[i] == carry[i]:
+#         answer.append('0')
+#         carry.append('0')
+#     elif num1[i] == '1' and num1[i] == num2[i] == carry[i]:
+#         answer.append('1')
+#         carry.append('1')
+#     elif (num1[i] == '1' and num2[i] == '1') or (num1[i] == '1' and carry[i] == '1') or (num2[i] == '1' and carry[i] == '1'):
+#         answer.append('0')
+#         carry.append('1')
+#     elif num1[i] == '1' or num2[i] == '1' or carry[i] == '1':
+#         carry.append('0')
+#         answer.append('1')
+# print(int(''.join(answer)[::-1],2))
+
+
+x = 127
+y = 63
+
+num1 = list(str('{0:08b}'.format(x)[::-1]))
+num2 = list(str('{0:08b}'.format(y)[::-1]))
+carry = [0]
+answer = []
+
+for i in range(len(num1)):
+    if num1[i] == '0' and num1[i] == num2[i] == carry[i]:
+        print('zero')
+        answer.append('0')
+        carry.append('0')
+    elif num1[i] == '1' and num1[i] == num2[i] == carry[i]:
+        print('one carry one')
+        answer.append('1')
+        carry.append('1')
+    elif (num1[i] == '1' and num2[i] == '1') or (num1[i] == '1' and carry[i] == '1') or (num2[i] == '1' and carry[i] == '1'):
+        print('zero carry one')
+        answer.append('0')
+        carry.append('1')
+    elif num1[i] == '1' or num2[i] == '1' or carry[i] == '1':
+        print('one')
+        carry.append('0')
+        answer.append('1')
+
+
+print('\n',''.join(num1), 'plus', ''.join(num2), 'equals', ''.join(answer) )
+
+print('or', int(''.join(answer)[::-1],2))
